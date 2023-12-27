9473955ea96bd2e0305d8514bb0ee2e1c36e04ed
balennouri <balennouri@gmail.com>
2023-12-27 23:40:52 +0100

    Feat: Add possibile ways to check for a win, check for draw and flip the player after every turn.

--- run.py
+++ run.py
@@ -34,41 +34,122 @@ def play_game():
      flip_player()
 
     if winner == "X" or winner == "O":
-       print(winner + "Has won the game.\n")
+       print(winner + " Has won the game.\n")
     elif winner == None:
-       print("Tie.\n")
-
+       print("It was a draw!\n")
 
 
 def handle_turn(player):
     """
     Handles the turn and put the markers for each player.
     """
-    position = input("Choose a position from 1-9\n")
+    print("Choose a position from 1-9\n")
+    position = input("It is " + current_player + " turn to choose:")
     position = int(position) - 1
 
-    board[position] = "X"
+    board[position] = player
     display_board()
 
     
 def check_if_game_over():
-   check_if_win()
+   check_for_win()
    check_if_tie()
 
 
-def check_if_win():
-   # check rows
-   # check columns
-   # check diagonals
+def check_for_win():
+   """
+   Check for possible wins.
+   """
+   global winner
+   row_winner = check_rows()
+   columns_winner = check_columns()
+   diagonals_winner = check_diagonals()
+   if row_winner:
+      winner = row_winner
+   elif columns_winner:
+      winner = columns_winner
+   elif diagonals_winner:
+      winner = diagonals_winner
+   else:
+      winner = None
+   return
+      
+
+def check_rows():
+   """
+   This checks if any of the rows have all the same value of X or O.
+   If it have the same it will return the winner.
+   """
+   global game_is_still_playing
+   row_1 = board[0] == board[1] == board[2] != "-"
+   row_2 = board[3] == board[4] == board[5] != "-"
+   row_3 = board[6] == board[7] == board[8] != "-"
+   if row_1 or row_2 or row_3:
+      game_is_still_playing = False
+   if row_1:
+      return board[0]
+   elif row_2:
+      return board[3]
+   elif row_3:
+      return board[6]
+   return
+   
+   
+def check_columns():
+   """
+   This checks if any of the columns have all the same value of X or O.
+   If it have the same it will return the winner.
+   """
+   global game_is_still_playing
+   columns_1 = board[0] == board[3] == board[6] != "-"
+   columns_2 = board[1] == board[4] == board[7] != "-"
+   columns_3 = board[2] == board[5] == board[8] != "-"
+   if columns_1 or columns_2 or columns_3:
+      game_is_still_playing = False
+   if columns_1:
+      return board[0]
+   elif columns_2:
+      return board[1]
+   elif columns_3:
+      return board[2]
    return
 
+       
+def check_diagonals():
+   """
+   This checks if any of the diagonals have all the same value of X or O.
+   If it have the same it will return the winner.
+   """
+   global game_is_still_playing
+   diagonals_1 = board[0] == board[4] == board[8] != "-"
+   diagonals_2 = board[6] == board[4] == board[2] != "-"
+   if diagonals_1 or diagonals_2:
+      game_is_still_playing = False
+   if diagonals_1:
+      return board[0]
+   elif diagonals_2:
+      return board[6]
+   return
+
+
 def check_if_tie():
+   global game_is_still_playing
+   if "-" not in board:
+      game_is_still_playing = False
    return
    
 
 def flip_player():
+   """
+   Change the players if current player is X
+   then change it to O.
+   """
+   global current_player
+   if current_player == "X":
+      current_player = "O"
+   elif current_player == "O":
+      current_player = "X"
    return
    
-
 
 play_game()
